69dd35165792d771a14de4abf3c46289bf3845ec
RealMasterChief117 <officialtrollfacexxx@gmail.com>
2025-10-31 16:11:20 -0400

    Update MumbleAccentSystem.cs

--- Content.Server/Speech/EntitySystems/MumbleAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/MumbleAccentSystem.cs
@@ -15,23 +15,6 @@ public sealed class MumbleAccentSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<MumbleAccentComponent, AccentGetEvent>(OnAccentGet);
-        SubscribeLocalEvent<MumbleAccentComponent, EmoteEvent>(OnEmote, before: [typeof(VocalSystem)]);
-    }
-
-    private void OnEmote(Entity<MumbleAccentComponent> ent, ref EmoteEvent args)
-    {
-        if (args.Handled || !args.Emote.Category.HasFlag(EmoteCategory.Vocal))
-            return;
-
-        if (TryComp<VocalComponent>(ent.Owner, out var vocalComp) && vocalComp.EmoteSounds is { } sounds)
-        {
-            // play a muffled version of the vocal emote
-            args.Handled = _chat.TryPlayEmoteSound(
-                ent.Owner,
-                _prototype.Index(sounds),
-                args.Emote,
-                ent.Comp.EmoteAudioParams);
-        }
     }
 
     public string Accentuate(string message, MumbleAccentComponent component)
